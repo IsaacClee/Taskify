9ec5b486e3398372a29806ad015ab5e5fa7d3963
IsaacClee <51291012+IsaacClee@users.noreply.github.com>
2022-10-20 17:42:50 -0500

    Ouch ouch ouch

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -36,6 +37,7 @@ const App = () => {
       <Routes>
           <Route exact path='/register' element={<Register />} />
           <Route exact path='/login' element={<Login />} />
+          <Route exact path='/dashboard' element={<Dashboard />} />
         </Routes>    
       </section>
     </Fragment>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
-        <Link to="/profiles">Developers</Link>
+        <Link to="/profiles"><i className="fa fa-plug"></i> Team Members</Link>
       </li>
       <li>
         <Link to="/posts">Tasks</Link>

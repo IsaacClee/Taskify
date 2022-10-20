12195f32fcdfcae2edab1410b204104c042b6111
IsaacClee <51291012+IsaacClee@users.noreply.github.com>
2022-10-20 13:39:55 -0500

    Checkpoint

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const Navbar = () => {
+
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+
+  const authLinks = (
+    <ul>
+      <li>
+        <Link to="/profiles">Developers</Link>
+      </li>
+      <li>
+        <Link to="/posts">Tasks</Link>
+      </li>
+      <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user" />{' '}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+    <li><a href="https://isaaclee.site"><i className="fa fa-paint-brush"></i> Creator</a></li>
+    <li>
+        <a onClick={logout} href="#!">
+          <i className="fas fa-sign-out-alt" />{' '}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+  </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+    <li><Link to="/profiles"><i className="fa fa-plug"></i> Team Members</Link></li>
+    <li><Link to="/register"><i className="fa fa-user-plus"></i> Create account</Link></li>
+    <li><Link to="/login"><i className="fa fa-user-circle"></i> Login</Link></li>
+    <li><a href="https://isaaclee.site"><i className="fa fa-paint-brush"></i> Creator</a></li>
+  </ul>
+  );
+
+
   return (
     <nav className="navbar bg-dark">
       <h1>
@@ -9,15 +48,19 @@ const Navbar = () => {
         <i className="fa fa-tasks" />  Taskify
         </Link>
       </h1>
-      <ul>
-        <li><Link to="/profiles"><i className="fa fa-plug"></i> Team Members</Link></li>
-        <li><Link to="/register"><i className="fa fa-user-plus"></i> Create account</Link></li>
-        <li><Link to="/login"><i className="fa fa-user-circle"></i> Login</Link></li>
-        <li><a href="https://isaaclee.site"><i className="fa fa-paint-brush"></i> Creator</a></li>
-      </ul>
+      { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>) }
     </nav>
-  )
+  );
+};
+
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
 }
 
-export default Navbar
+const mapStateToProps = state => ({ 
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, { logout })(Navbar);
 
